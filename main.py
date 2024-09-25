3a516b9128e2d265716ddc1c475923b7819eac4b
vedanshipanchal <vedanshipanchal@gmail.com>
2024-09-25 20:34:18 +0100

    Checking Alphabets

---
+++ main.py
@@ -0,0 +1,6 @@
+character = input("Please enter a character: ")
+
+if (character >= 'a' and character <= 'z'):
+    print("The character is an alphabet.")
+else:
+    print("The character is not an alphabet.")
